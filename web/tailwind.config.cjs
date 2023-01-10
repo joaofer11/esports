7102686fd7362cb5a4b150fac14d56cdf37b1bc9
Joao Fernandes <joaofergear003@gmail.com>
2023-01-10 19:44:40 -0300

    feat(web): add 'Inter' font-family

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,11 +5,14 @@ module.exports = {
   	'./index.html',
   ],
   theme: {
+  	fontFamily: {
+  		sans: ['Inter', 'sans-serif'],
+  	},
     extend: {
     	backgroundImage: {
     		galaxy: "url('/images/galaxy-background.png')",
     		'nlw-gradient': 'linear-gradient(89.85deg, #9572FC, #43E7AD, #E1D55D)',
-    		'game-gradient': 'linear-gradient(180deg, transparent 0%, rgba(0, 0, 0, 0.9) 67.88%)',
+    		'game-gradient': 'linear-gradient(180deg, transparent 0, rgba(0, 0, 0, 0.9) 67.88%)',
     	}
     },
   },
