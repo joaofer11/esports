8c0705ff78d7d70751370d147a6944488e91f1d6
Joao Fernandes <joaofergear003@gmail.com>
2023-01-10 19:32:07 -0300

    feat(web): create landing page layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,79 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
+import { LogoNlwESports } from './styles/svg-components/LogoNlwESports'
+
 export const App = () => {
 	return (
-		<>Hello World!</>
+		<div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+			<LogoNlwESports />
+			
+			<h1 className="text-6xl text-white font-black mt-20 mb-16">
+				Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
+			</h1>
+			
+			<div className="grid grid-cols-6 gap-6">
+				<a href="" className="relative rounded-lg overflow-hidden">
+					<img src="/images/game-1.png" alt="" />
+					
+					<div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0">
+						<strong className="font-bold text-white block">League of Legends</strong>
+						<span className="text-zinc-300 text-sm block">4 anúncios</span>
+					</div>
+				</a>
+				<a href="" className="relative rounded-lg overflow-hidden">
+					<img src="/images/game-2.png" alt="" />
+					
+					<div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0">
+						<strong className="font-bold text-white block">League of Legends</strong>
+						<span className="text-zinc-300 text-sm block">4 anúncios</span>
+					</div>
+				</a>
+				<a href="" className="relative rounded-lg overflow-hidden">
+					<img src="/images/game-3.png" alt="" />
+					
+					<div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0">
+						<strong className="font-bold text-white block">League of Legends</strong>
+						<span className="text-zinc-300 text-sm block">4 anúncios</span>
+					</div>
+				</a>
+				<a href="" className="relative rounded-lg overflow-hidden">
+					<img src="/images/game-4.png" alt="" />
+					
+					<div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0">
+						<strong className="font-bold text-white block">League of Legends</strong>
+						<span className="text-zinc-300 text-sm block">4 anúncios</span>
+					</div>
+				</a>
+				<a href="" className="relative rounded-lg overflow-hidden">
+					<img src="/images/game-5.png" alt="" />
+					
+					<div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0">
+						<strong className="font-bold text-white block">League of Legends</strong>
+						<span className="text-zinc-300 text-sm block">4 anúncios</span>
+					</div>
+				</a>
+				<a href="" className="relative rounded-lg overflow-hidden">
+					<img src="/images/game-6.png" alt="" />
+					
+					<div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0">
+						<strong className="font-bold text-white block">League of Legends</strong>
+						<span className="text-zinc-300 text-sm block">4 anúncios</span>
+					</div>
+				</a>
+			</div>
+			
+			<div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+				<div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+					<div className="flex flex-col">
+						<strong className="text-2xl text-white font-black">Não encontrou seu duo?</strong>
+						<span className="text-zinc-400">Publique um anúncio para encontrar novos players!</span>
+					</div>
+					
+					<button className="py-3 px-4 bg-violet-500 transition-color duration-300 ease hover:bg-violet-600 text-white rounded flex items-center gap-3">
+						<MagnifyingGlassPlus size={24} />
+						Publicar anúncio
+					</button>
+				</div>
+			</div>
+		</div>
 	)
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,9 +2,16 @@
 module.exports = {
   content: [
   	'./src/**/*.tsx',
+  	'./index.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+    	backgroundImage: {
+    		galaxy: "url('/images/galaxy-background.png')",
+    		'nlw-gradient': 'linear-gradient(89.85deg, #9572FC, #43E7AD, #E1D55D)',
+    		'game-gradient': 'linear-gradient(180deg, transparent 0%, rgba(0, 0, 0, 0.9) 67.88%)',
+    	}
+    },
   },
   plugins: [],
 }
