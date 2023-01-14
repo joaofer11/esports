1fb09fefc25cba75469d2cede491c864568b3862
João Fernandes <joaofergear003@gmail.com>
2023-01-14 14:57:10 -0300

    feat(server/database): implement a count record of ads for each game
    create the first ad as a preview for testing

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,23 @@
-import express from 'express';
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
-
-app.get('/ads', (req, res) => {
-	return res.json([
-		{ id: 1, anúncio: 'anúncio 1' },
-		{ id: 2, anúncio: 'anúncio 2' },
-		{ id: 3, anúncio: 'anúncio 3' },
-		{ id: 4, anúncio: 'anúncio 4' },
-	])
+const prisma = new PrismaClient({
+  log: ["query"],
 });
 
-app.listen(3333)
+app.get("/games", async (req, res) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+
+  return res.json(games);
+});
+
+app.listen(3333);
