8f87588e757c6aac3a223d142d1b57f85635327d
João Fernandes <joaofergear003@gmail.com>
2023-01-15 18:52:35 -0300

    feat(mobile): start to fetch and list games from server

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,44 +1,34 @@
 import {
-	Text,
-	ImageBackground, 
-	TouchableOpacity,
-	ImageSourcePropType,
-	TouchableOpacityProps,
-} from 'react-native';
-import { styles } from './styles';
-import { THEME } from '../../theme';
-import { LinearGradient } from 'expo-linear-gradient';
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  ImageSourcePropType,
+  TouchableOpacityProps,
+} from "react-native";
+import { styles } from "./styles";
+import { THEME } from "../../theme";
+import { LinearGradient } from "expo-linear-gradient";
 
 export interface GameCardProps {
-	id: string;
-	ads: string;
-	name: string;
-	cover: ImageSourcePropType;
-};
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  };
+}
 
 interface Props extends TouchableOpacityProps {
-	data: GameCardProps;
-};
+  data: GameCardProps;
+}
 
 export const GameCard = ({ data, ...rest }: Props) => (
-	<TouchableOpacity style={styles.container} {...rest}>
-		<ImageBackground 
-			source={data.cover}
-			style={styles.cover}
-		>
-		
-			<LinearGradient
-				style={styles.footer}
-				colors={THEME.COLORS.FOOTER}
-			>
-				<Text style={styles.name}>
-					{data.name}
-				</Text>
-				
-				<Text style={styles.ads}>
-					{data.ads} anúncios
-				</Text>
-			</LinearGradient>
-		</ImageBackground>
-	</TouchableOpacity>
+  <TouchableOpacity style={styles.container} {...rest}>
+    <ImageBackground source={{ uri: data.bannerUrl }} style={styles.cover}>
+      <LinearGradient style={styles.footer} colors={THEME.COLORS.FOOTER}>
+        <Text style={styles.name}>{data.title}</Text>
+        <Text style={styles.ads}>{data._count.ads} anúncios</Text>
+      </LinearGradient>
+    </ImageBackground>
+  </TouchableOpacity>
 );
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,32 +1,48 @@
-import { styles } from './styles';
-import { GAMES } from '../../data/games';
-import { Heading } from '../../components/Heading';
-import { GameCard } from '../../components/GameCard';
-import { View, Image, FlatList } from 'react-native';
-import logoImg from '../../assets/logo-nlw-esports.png';
+import { styles } from "./styles";
+import { useState, useEffect } from "react";
+import { Heading } from "../../components/Heading";
+import { GameCard } from "../../components/GameCard";
+import { View, Image, FlatList } from "react-native";
+import logoImg from "../../assets/logo-nlw-esports.png";
 
-export const Home = () => (
-	<View style={styles.container}>
-		<Image
-			source={logoImg}
-			style={styles.logoImg}
-		/>
-		
-		<Heading 
-			title="Encontre seu duo!"
-			subtitle="Selecione o game que você deseja jogar..."
-		/>
-		
-		<FlatList 
-			data={GAMES}
-			keyExtractor={item => item.id}
-			renderItem={({ item: game }) => (
-				<GameCard data={game} />
-			)}
-			
-			horizontal
-			showsHorizontalScrollIndicator={false}
-			contentContainerStyle={styles.gamesList}
-		/>
-	</View>
-);
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  };
+}
+
+export const Home = () => {
+  const [gamesList, setGamesList] = useState<Game[]>([]);
+
+  const fetchGamesList = async () => {
+    const data = await (await fetch("http://127.0.0.1:3333/games")).json();
+    setGamesList(data);
+  };
+
+  useEffect(() => {
+    fetchGamesList();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logoImg} />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que você deseja jogar..."
+      />
+
+      <FlatList
+        data={gamesList}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: gameItem }) => <GameCard data={gameItem} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.gamesList}
+      />
+    </View>
+  );
+};
